2ce39218cac0fca3a814114856874ed0c71cbae9
Diego Diniz <diego.diniz@dasa.com.br>
2019-10-02 14:04:29 -0300

    [GPA-145] commit initial

---
+++ src/main/java/br/com/dasa/apitermo/ApiTermoApplication.java
@@ -0,0 +1,13 @@
+package br.com.dasa.apitermo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTermoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTermoApplication.class, args);
+	}
+
+}
